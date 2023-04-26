0a48f563f7954e0da7a2f1d896729040d067a2f8
EseAH <ese.aquino@gmail.com>
2023-04-25 21:55:22 -0300

    title - input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Input from './components/Input/input'
+import Title from './components/Title/title'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>To do</h1>
+      <div className='bg-gray-900 min-h-screen h-full text-gray-100 flex items-center justify-center py-20 px-5'>
+        <div className='container flex flex-col max-w-xl'>
+          <Title/>
+          <Input/>
+        </div>
       </div>
     </>
   )
