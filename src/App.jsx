4bc5899d687ce5425f1486efb45ca50711b49992
EseAH <ese.aquino@gmail.com>
2023-04-27 11:43:21 -0300

    6

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,47 @@ function App() {
       completed: false,
     },
   ])
+
+  
+
+  const addTodo = (title) => {
+    const lastId = todos.length > 0 ? todos[todos.length - 1].id : 1;
+    const newTodo = {
+      id: lastId + 1,
+      title,
+      completed: false
+    }
+    const todoList = [...todos]
+    todoList.push(newTodo)
+    setTodos(todoList)
+  }
+
+  const handleSetComplete = (id) => {
+    const updatedList = todos.map(todo => {
+      if(todo.id === id) {
+        return {...todo, completed: !todo.completed}
+      }
+      return todo
+    })
+    setTodos(updatedList)
+  }
+
+  const handleDelete = (id) => {
+    const updatedList = todos.filter(todo => todo.id !== id)
+    setTodos(updatedList)
+  }
+
   return (
     <>
       <div className='bg-gray-900 min-h-screen h-full font-inter text-gray-100 flex items-center justify-center py-20 px-5'>
         <div className='container flex flex-col max-w-xl'>
           <Title/>
-          <Input/>
-          <TasksList todos={todos}/>
+          <Input addTodo={addTodo}/>
+          <TasksList 
+            todos={todos}
+            handleSetComplete={handleSetComplete}
+            handleDelete={handleDelete}
+          />
         </div>
       </div>
     </>
--- src/components/TasksList/tasksList.jsx
+++ src/components/TasksList/tasksList.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import TodoFilter from '../TodoFilters/todoFilter'
 import Todo from '../Todo/todo'
 
-const TasksList = ({todos}) => {
+const TasksList = ({ todos, handleSetComplete, handleDelete }) => {
   return (
     <div className='flex flex-col mt-7 rounded-lg overflow-hidden shadow-2xl'>
         {todos.map(todo => {
           return (
-            <Todo key={todo.id} todo={todo}/>
+            <Todo 
+              key={todo.id} 
+              todo={todo} 
+              handleSetComplete={handleSetComplete}
+              handleDelete={handleDelete}
+            />
           )
         })}
         <TodoFilter/>
